3d24e46016ab5dc1958f15f2928bda44209d78d3
Lucas Raoni Hideki Antunes <85591742+mahauni@users.noreply.github.com>
2024-04-24 11:03:28 -0300

    Adding features to login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _passwordVisible = false;
+  bool _denySpaces = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,25 +27,50 @@ class _LoginPageState extends State<LoginPage> {
             child: Container(
               alignment: Alignment.center,
               child: ListView(
+                padding: EdgeInsets.all(10.0),
                 shrinkWrap: true,
                 children: [
                   Text("E-mail"),
                   TextField(
+                    inputFormatters: [
+                      if (_denySpaces)
+                        FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                    ],
                     decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Enter the email here"),
+                      border: OutlineInputBorder(),
+                      hintText: "Enter the email here",
+                    ),
                   ),
                   SizedBox(
                     height: 15,
                   ),
                   Text("Senha"),
                   TextField(
+                    inputFormatters: [
+                      if (_denySpaces)
+                        FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                    ],
                     decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Enter the email here"),
+                      border: OutlineInputBorder(),
+                      hintText: "Enter the email here",
+                      suffixIcon: IconButton(
+                        splashColor: Colors.transparent,
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                        ),
+                        iconSize: 20,
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                    ),
                     autocorrect: false,
                     enableSuggestions: false,
-                    obscureText: true,
+                    obscureText: !_passwordVisible,
                   ),
                   SizedBox(
                     height: 30,
